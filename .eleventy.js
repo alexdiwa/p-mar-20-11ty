86f6b57267e7add5018933fb96f7b8db77cbed33
Alex Diwa <atdiwa@gmail.com>
2020-03-29 00:41:18 +1100

    add responsive images

--- .eleventy.js
+++ .eleventy.js
@@ -1,9 +1,7 @@
 const htmlmin = require("html-minifier");
-const lazyImagesPlugin = require("eleventy-plugin-lazyimages");
 const fs = require("fs");
 
 module.exports = function(eleventyConfig) {
-  eleventyConfig.addPlugin(lazyImagesPlugin);
   eleventyConfig.addTransform("htmlmin", function(content, outputPath) {
     if( outputPath.endsWith(".html") ) {
       let minified = htmlmin.minify(content, {
@@ -23,10 +21,7 @@ module.exports = function(eleventyConfig) {
         const content_404 = fs.readFileSync('_site/404.html');
 
         bs.addMiddleware("*", (req, res) => {
-          // Provides the 404 content without redirect.
           res.write(content_404);
-          // Add 404 http status code in request header.
-          // res.writeHead(404, { "Content-Type": "text/html" });
           res.writeHead(404);
           res.end();
         });
