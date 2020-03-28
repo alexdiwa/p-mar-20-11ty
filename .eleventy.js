9abbf9c2755b81b4e661367038eb70e15705ec77
Alex Diwa <atdiwa@gmail.com>
2020-03-28 15:06:14 +1100

    add img lazy load

--- .eleventy.js
+++ .eleventy.js
@@ -1,6 +1,8 @@
 const htmlmin = require("html-minifier");
+const lazyImagesPlugin = require("eleventy-plugin-lazyimages");
 
 module.exports = function(eleventyConfig) {
+  eleventyConfig.addPlugin(lazyImagesPlugin);
   eleventyConfig.addTransform("htmlmin", function(content, outputPath) {
     if( outputPath.endsWith(".html") ) {
       let minified = htmlmin.minify(content, {
